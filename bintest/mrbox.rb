53c2a23526cec5163704b36f9cf0f37092ea344b
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-12-21 23:39:24 +0900

    [WIP] Add bintest

--- bintest/mrbox.rb
+++ bintest/mrbox.rb
@@ -2,14 +2,14 @@ require 'open3'
 
 BIN_PATH = File.join(File.dirname(__FILE__), "../mruby/bin/mrbox")
 
-assert('hello') do
+assert('mrbox') do
   output, status = Open3.capture2(BIN_PATH)
 
   assert_true status.success?, "Process did not exit cleanly"
   assert_include output, "Hello World"
 end
 
-assert('version') do
+assert('mrbox help') do
   output, status = Open3.capture2(BIN_PATH, "version")
 
   assert_true status.success?, "Process did not exit cleanly"
